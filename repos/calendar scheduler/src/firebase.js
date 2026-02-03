267dacf78ce1f0411bcf7978504dfca9333059b0
a.huynh <alexhuynh1999@gmail.com>
2026-02-03 09:30:43 -0800

    hide secrets

--- repos/calendar scheduler/src/firebase.js
+++ repos/calendar scheduler/src/firebase.js
@@ -4,19 +4,12 @@ import { getFirestore } from "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
 // You can find this in the Firebase Console -> Project Settings -> General -> Your apps
 const firebaseConfig = {
-
-    apiKey: "AIzaSyB7oHTGIM61ZIuRqi9fWMqzAzNpgZESlQg",
-
-    authDomain: "calendar-scheduler-beae1.firebaseapp.com",
-
-    projectId: "calendar-scheduler-beae1",
-
-    storageBucket: "calendar-scheduler-beae1.firebasestorage.app",
-
-    messagingSenderId: "517635746650",
-
-    appId: "1:517635746650:web:9f6fe403c3b093371f2163"
-
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 
